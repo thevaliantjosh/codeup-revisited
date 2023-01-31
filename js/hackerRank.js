ff9a354b1355e6ede67583719905765affde6cc5
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2023-01-31 08:58:44 -0600

    Completed the timeConversion function for hackerrank

--- js/hackerRank.js
+++ js/hackerRank.js
@@ -31,7 +31,7 @@ function plusMinus(arr){
 
 }
 
-console.log(plusMinus([5, 4, 1, 0, 0, -4, -1, -4]));
+// console.log(plusMinus([5, 4, 1, 0, 0, -4, -1, -4]));
 
 
 /*PROBLEM 2
@@ -52,7 +52,7 @@ function miniMaxSum(arr){
         return sortedItem1 - sortedItem2;
 
     })
-    console.log(sortedNumbersArray);
+    // console.log(sortedNumbersArray);
     let lowestFourTotal = 0;
     let highestFourTotal = 0;
     for (let i = 0; i < sortedNumbersArray.length; i++){
@@ -68,7 +68,55 @@ function miniMaxSum(arr){
     console.log(`${lowestFourTotal} ${highestFourTotal}`);
 }
 
-console.log(miniMaxSum(shortNumbersArray));
+// console.log(miniMaxSum(shortNumbersArray));
+//
+//
+// console.log(miniMaxSum(numbersArray));
 
+/*PROBLEM 3
+* Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 
-console.log(miniMaxSum(numbersArray));
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+* s = '12:01:00PM
+Return: '12:01:00'.
+
+* s = '12:01:00AM'
+Return: '00:01:00'.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in  hour*/
+
+function timeConversion(time){
+    let twelve = parseInt("12");
+    let hours = parseInt(time.slice(0, 2))
+    let minutes = time.slice(3, 5);
+    let seconds = time.slice(6, 8);
+    let afterMidnight = time.substring(8, 10) === "AM";
+    let afterNoon = time.substring(8, 10) === "PM";
+    if (parseInt(time) === twelve && afterMidnight){
+        let midnight = hours - twelve;
+        return (`${midnight}0:${minutes}:${seconds}`);
+    } else if (parseInt(time) === twelve && afterNoon ) {
+        return `${hours}:${minutes}:${seconds}`
+    } else if (afterMidnight){
+        return `0${hours}:${minutes}:${seconds}`
+    } else {
+       return (`${(hours + twelve)}:${minutes}:${seconds}`);
+    }
+}
+//
+// console.log(timeConversion("01:00:00AM"));//good
+// console.log(timeConversion("12:00:00AM"));//good
+// console.log(timeConversion("02:35:45AM"));//good
+// console.log(timeConversion("12:35:45PM"));//good
+
+console.log(timeConversion("06:40:03AM"));
